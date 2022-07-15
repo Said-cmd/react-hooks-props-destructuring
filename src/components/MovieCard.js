eed84134b336787450300df25758304e10f856af
Said Hussein <saidhussein12342@gmail.com>
2022-07-15 18:39:37 +0300

    Done.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,19 +1,6 @@
 import React from "react";
 
-function App () {
-  const title = "Mad Max";
-  const posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg";
-  const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"]
-
-  return (
-    <div className="App">
-      <MovieCard title={title} posterSrc={posterURL} genres={genresArr} />
-    </div>
-  );
-}
-
-function MovieCard (title, posterSrc, genres) {
-{
+function MovieCard({ title, posterSrc, genres }) {
   return (
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
@@ -21,7 +8,6 @@ function MovieCard (title, posterSrc, genres) {
       <small>{genres.join(", ")}</small>
     </div>
   );
-}
 }
 
 export default MovieCard;
